f1852ebeefd440438a9c023ca715764d6d978ca6
Manish Shivabhakti <manish.shivabhakti.99@gmail.com>
2025-01-15 23:16:52 +0545

    added Home

---
+++ 🏠_Home.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="AI Veda",
+    page_icon="🌿",
+)
+
+st.title('Super Chat')
+st.subheader(
+    'Let everything talk!'
+    )
+st.write('This is a chatbot that can talk about anything you want. Just ask a question and it will answer you. '
+         'If you want to know more about a specific topic, you can ask the chatbot to search for it in the web. '
+         'You can also ask the chatbot to tell you a joke or a riddle. Have fun!')
