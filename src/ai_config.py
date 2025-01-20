06a385c32f494400cbd62c3a0421ac793c88131a
Manish Shivabhakti <manish.shivabhakti.99@gmail.com>
2025-01-20 22:33:14 +0545

    refactor: update chunk creation to process sitemap and fetch article content

--- src/ai_config.py
+++ src/ai_config.py
@@ -1,42 +1,51 @@
 from .session import SESSION as S
 from .session import SVC as SVC
 import streamlit as st
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from .preprocess import get_urls_from_sitemap, fetch_article_content
 
 NUM_CHUNKS = 5
 COLUMNS = ["chunk_text", "source_url", "bot_id"]
 
-S.sql("USE ROLE ACCOUNTADMIN").collect()
-S.sql("""
-    CREATE OR REPLACE TABLE CHUNKS (
-        chunk_id INTEGER AUTOINCREMENT START 1 INCREMENT 1 PRIMARY KEY,
-        bot_id INTEGER NOT NULL REFERENCES BOTS(bot_id) ON DELETE CASCADE,
-        source_url VARCHAR(16777216) NOT NULL,
-        chunk_text TEXT NOT NULL,
-        created_at TIMESTAMP_NTZ DEFAULT CURRENT_TIMESTAMP()
-    )
-""").collect()
 
-def create_chunk(source_url, chunk_text, bot_id):
+def create_chunk(bot_id, sitemap_url):
     """
-    Create a chunk with auto-generated ID
+    process_sitemap
     """
-    S.sql(
-        """
-        INSERT INTO CHUNKS (bot_id, source_url, chunk_text)
-        values (%s, %s, %s)
-        """,
-        (bot_id, source_url, chunk_text)
-    ).collect()
-    print("Chunk created successfully")
+    if "sitemap.xml" in sitemap_url:
+        urls = get_urls_from_sitemap(sitemap_url)
+    else:
+        urls = sitemap_url.split(",")
+    for url in urls:
+        content = fetch_article_content(url)
+        if content:
+            # split the content using text splitter
+            text_splitter = RecursiveCharacterTextSplitter(
+                chunk_size=512, chunk_overlap=128)
+            chunks = text_splitter.split_text(content['content'])
+
+            for chunk in chunks:
+                print(chunk)
+                # store it into the database
+                S.sql(
+                    """
+                    INSERT INTO CHUNKS (bot_id, source_url, chunk_text)
+                    values (?, ?, ?)
+                    """,
+                    params=[bot_id, content["source_url"], chunk]
+                ).collect()
+                S.sql("COMMIT").collect()
+                print("Chunk created successfully")
+
 
 def get_similar_chunks(query):
     """
     Get similar chunks to the query
     """
-    filter_obj = {"@eq": {"bot_id": st.query_params["bot_id"]} }
+    filter_obj = {"@eq": {"bot_id": st.query_params["bot_id"]}}
     response = SVC.search(query, COLUMNS, filter=filter_obj, limit=NUM_CHUNKS)
     print(response)
-    return response.model_dump_json()  
+    return response.model_dump_json()
 
 
 def generate_prompt(question):
@@ -64,9 +73,3 @@ def generate_prompt(question):
            """
     print("prompt: ", prompt)
     print("prompt context: ", prompt_context)
-
-    
-
-
-
-
