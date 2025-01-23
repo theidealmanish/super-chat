b9f2c59b6462af891be9f48857970e2181b90f06
Manish Shivabhakti <manish.shivabhakti.99@gmail.com>
2025-01-23 08:48:52 +0545

    keep session alive

--- src/session.py
+++ src/session.py
@@ -7,16 +7,18 @@ load_dotenv()
 
 # service parameters
 CONNECTION_PARAMS = {
-  "account": os.environ.get("SNOWFLAKE_ACCOUNT"),
-  "user": os.environ.get("SNOWFLAKE_USER"),
-  "password": os.environ.get("SNOWFLAKE_USER_PASSWORD"),
-  "role": os.environ.get("SNOWFLAKE_ROLE"),
-  "database": os.environ.get("SNOWFLAKE_DATABASE"),
-  "schema": os.environ.get("SNOWFLAKE_SCHEMA"),
-  "warehouse": os.environ.get("SNOWFLAKE_WAREHOUSE"),
-  "search_service": os.environ.get("SNOWFLAKE_CORTEX_SEARCH_SERVICE"),
+    "account": os.environ.get("SNOWFLAKE_ACCOUNT"),
+    "user": os.environ.get("SNOWFLAKE_USER"),
+    "password": os.environ.get("SNOWFLAKE_USER_PASSWORD"),
+    "role": os.environ.get("SNOWFLAKE_ROLE"),
+    "database": os.environ.get("SNOWFLAKE_DATABASE"),
+    "schema": os.environ.get("SNOWFLAKE_SCHEMA"),
+    "warehouse": os.environ.get("SNOWFLAKE_WAREHOUSE"),
+    "search_service": os.environ.get("SNOWFLAKE_CORTEX_SEARCH_SERVICE"),
+    "client_session_keep_alive": True
 }
 
 
-SESSION = Session.builder.configs(CONNECTION_PARAMS).create()                      
-SVC = Root(SESSION).databases[CONNECTION_PARAMS["database"]].schemas[CONNECTION_PARAMS["schema"]].cortex_search_services[CONNECTION_PARAMS["search_service"]]
+SESSION = Session.builder.configs(CONNECTION_PARAMS).create()
+SVC = Root(SESSION).databases[CONNECTION_PARAMS["database"]].schemas[CONNECTION_PARAMS["schema"]
+                                                                     ].cortex_search_services[CONNECTION_PARAMS["search_service"]]
